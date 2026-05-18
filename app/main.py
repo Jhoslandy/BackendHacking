aa3cd3d30d77e8e4724a6a388779ce57ea731c75
Jhoslandy <aruquipacjose@gmail.com>
2026-05-18 00:44:01 -0400

    Fix CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # IMPORTACIÓN NUEVA
 
 from app.api.router import api_router
 from app.core.config import get_settings
@@ -10,6 +11,17 @@ app = FastAPI(
     description=settings.APP_DESCRIPTION,
     version=settings.APP_VERSION,
 )
+
+# --- INICIO CONFIGURACIÓN CORS ---
+# Esto permite que tu frontend en Render o en localhost se conecte sin bloqueos
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # En un entorno real se pone la URL exacta del frontend, aquí ponemos "*" para evitar problemas en la demo.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# --- FIN CONFIGURACIÓN CORS ---
 
 app.include_router(api_router)
 
